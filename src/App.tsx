b812c23e64567f6014ffe691eab05ea4cfdf32e1
Kasun Lakshitha <kasun2014@outlook.com>
2025-02-15 23:51:37 +0530

    Base layout is done, react router implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import Dashboard from './components/dashboard/dashboard';
+// First install react-router-dom: npm install react-router-dom @types/react-router-dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Navbar from './components/navbar/navbar';
 import Sidebar from './components/sidebar/sidebar';
 import ContentLayout from './layout/content-layout';
 import Layout from './layout/layout';
+import Home from './pages/home/home';
+import Settings from './pages/settings/settings';
 
 function App() {
 	return (
-		<Layout className='min-h-screen bg-black pt-4 pl-8 pr-4'>
-			<Navbar />
-			<ContentLayout>
-				<Sidebar />
-				<Dashboard />
-			</ContentLayout>
-		</Layout>
+		<Router>
+			<Layout className='min-h-screen bg-black pt-4 pl-8 pr-4'>
+				<Navbar />
+				<ContentLayout>
+					<Sidebar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/settings" element={<Settings />} />
+					</Routes>
+				</ContentLayout>
+			</Layout>
+		</Router>
 	);
 }
 
