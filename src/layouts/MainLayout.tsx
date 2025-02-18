28421fc895fe2e082da774d1a3df78b0e2187aec
Kasun Lakshitha <kasun2014@outlook.com>
2025-02-18 23:52:59 +0530

    Add Sidebar to MainLayout and set dark background

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import Navbar from '../components/navbar/navbar';
+import Sidebar from '../components/sidebar/sidebar';
 
 const MainLayout = () => {
 	return (
-		<div>
+		<div className='min-h-screen'>
 			<Navbar />
 			<main>
+				<Sidebar />
 				<Outlet />
 			</main>
 		</div>
