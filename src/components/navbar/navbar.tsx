f02d64e8e83fa3a801ec8d990d9f0350ce2db0d3
Kasun Lakshitha <kasun2014@outlook.com>
2025-02-25 23:59:12 +0530

    Implement Navbar with navigation links and styling

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,3 +1,42 @@
+import { Link } from 'react-router';
+import logo from '../../assets/icons/app-logo.svg';
+
 export default function Navbar() {
-	return <div>navbar</div>;
+	return (
+		<header className='py-4 px-6'>
+			<nav className='container mx-auto flex items-center justify-between'>
+				<Link to='/'>
+					<img src={logo} alt='' />
+				</Link>
+
+				<menu className='flex items-center gap-4'>
+					<Link
+						to='/'
+						className='bg-dark-button-bg px-4 py-2 rounded-full'
+					>
+						Overview
+					</Link>
+					<Link
+						to='/'
+						className='bg-dark-button-bg px-4 py-2 rounded-full'
+					>
+						Performance
+					</Link>
+					<Link
+						to='/'
+						className='bg-dark-button-bg px-4 py-2 rounded-full'
+					>
+						Activity
+					</Link>
+					<Link
+						to='/'
+						className='bg-dark-button-bg px-4 py-2 rounded-full'
+					>
+						Deals
+					</Link>
+				</menu>
+				
+			</nav>
+		</header>
+	);
 }
