091f6aa3f298ad882a86cd6cff0433ab3c1cdb66
Kasun Lakshitha <kasun2014@outlook.com>
2025-03-01 23:55:57 +0530

    Add Navbar icons and header styling improvements

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router';
 import logo from '../../assets/icons/app-logo.svg';
+import settingsIcon from '../../assets/icons/settings.svg';
+import bellIcon from '../../assets/icons/bell.svg';
+import avatar from '../../assets/avatar.svg';
 
 export default function Navbar() {
 	return (
 		<header className='py-4 px-6'>
-			<nav className='container mx-auto flex items-center justify-between'>
+			<nav className='container min-w-full flex items-center justify-between'>
 				<Link to='/'>
 					<img src={logo} alt='' />
 				</Link>
@@ -35,7 +38,23 @@ export default function Navbar() {
 						Deals
 					</Link>
 				</menu>
-				
+				<menu className='flex items-center gap-4'>
+					<Link
+						to='/'
+						className='flex items-center justify-center bg-dark-button-bg w-[48px] h-[48px] rounded-full'
+					>
+						<img src={settingsIcon} alt='settings_icon' />
+					</Link>
+					<Link
+						to='/'
+						className='flex items-center justify-center bg-dark-button-bg w-[48px] h-[48px] rounded-full'
+					>
+						<img src={bellIcon} alt='bell_icon' />
+					</Link>
+					<button className='bg-dark-button-bg w-[48px] h-[48px] rounded-full overflow-hidden'>
+						<img src={avatar} alt='avatar' />
+					</button>
+				</menu>
 			</nav>
 		</header>
 	);
