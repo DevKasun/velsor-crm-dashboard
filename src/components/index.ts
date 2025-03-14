6a64fee88e96d3541f6136ce30cdd1e3442810f3
Kasun Lakshitha <kasun2014@outlook.com>
2025-03-14 23:17:05 +0530

    Clean up component files and styling
    The changes mainly involve improving code formatting, adding consistent border
    styling to navbar/sidebar, and cleaning up imports. Since this is a fairly
    small, focused set of changes, I'm providing just a subject line that captures
    the essence of the changes.

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import Sidebar from './sidebar';
-import Button from './button';
-import Dashboard from './dashboard';
-import Navbar from './navbar';
-
-export {
-    Sidebar, Button, Dashboard, Navbar
-}
-
